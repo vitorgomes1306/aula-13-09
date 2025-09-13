178ed4e941dd8faa54e2eb323cf94a07fa1c1a70
JohnDoe <apollocreed9555@gmail.com>
2025-09-13 12:52:35 -0300

    Chamando dependêncis

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-//inciando projeto novo com node, express, mysql e body-parser
+// Projeto CRUD com Node.js, Express e MySQL
+//===============================================
+
+//chamando as dependencias 
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+
+//inciciando o express
 const app = express();
+
+// definindo a porta
 const port = 3000;
 
 // Configuração do body-parser
@@ -11,16 +18,41 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Configuração da conexão com o banco de dados MySQL
-const db = mysql.createConnection({
-    host: 'localhost
-    user: 'root',
-    password: '',
-    database: 'aula'
-});
-db.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
-        return;
-    }       
-    console.log('Conectado ao banco de dados MySQL.');
+// // Configuração do banco de dados MySQL
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'aula'
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Erro ao conectar ao banco de dados:', err);
+//         return;
+//     }
+//     console.log('Conectado ao banco de dados MySQL.');
+// });
+// // Rota para exibir o formulário
+// app.get('/', (req, res) => {
+//     res.render('form');
+// });
+
+// // Rota para processar o formulário
+// app.post('/submit', (req, res) => {
+//     const { name, email } = req.body;
+//     const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
+//     db.query(sql, [name, email], (err, result) => {
+//         if (err) {
+//             console.error('Erro ao inserir dados:', err);
+//             res.status(500).send('Erro ao inserir dados');
+//             return;
+//         }
+//         console.log('Dados inseridos com sucesso:', result);
+//         res.redirect('/');
+//     });
+// });
+
+// app.listen(port, () => {
+//     console.log(`Servidor rodando em http://localhost:${port}`);
+// });
