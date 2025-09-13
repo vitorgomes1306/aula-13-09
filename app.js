1c51f633b322f41741b2523f5a7debc1701f78a9
JohnDoe <apollocreed9555@gmail.com>
2025-09-13 12:44:49 -0300

    Inicio do projeto com alterações

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//inciando projeto novo
+//inciando projeto novo com node, express, mysql e body-parser
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// Configuração do body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Configuração da conexão com o banco de dados MySQL
+const db = mysql.createConnection({
+    host: 'localhost
+    user: 'root',
+    password: '',
+    database: 'aula'
+});
+db.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }       
+    console.log('Conectado ao banco de dados MySQL.');
